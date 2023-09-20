226e0132d7a7fc8a9828b9c948ade6b39a3d638b
Rob Mitchell <rlmitchell@gmail.com>
2023-09-20 15:07:13 -0500

    api/index.php

--- api/index.php
+++ api/index.php
@@ -1,13 +1,14 @@
 <?php
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json');
 
 if($_SERVER['REQUEST_URI'] == '/api/docs' || $_SERVER['REQUEST_URI'] == '/api/'){
+  header('Content-Type: text/html');
   include_once('docs.php');
   exit();
 }
 
 if(str_starts_with($_SERVER['REQUEST_URI'], '/api/scrape') ){
+  header('Content-Type: application/json');
   include_once('scrape.php');
   exit();
 }
