30c30e0cc45601d8cddeadfb3f396ca1175b8549
Rob Mitchell <rlmitchell@gmail.com>
2023-09-20 20:48:45 -0500

    api/docs.php

---
+++ api/docs.php
@@ -0,0 +1,13 @@
+<?php //
+?>
+<html><body>
+finviz-scraper: small api for getting price and annual dividend information from finviz.com.  Alternative to using a browser. 
+<br><br><br>
+/api - this doc
+<br><br>
+/api/docs - this doc
+<br><br>
+/api/scrape/&lt;symbol&gt;<br>
+returns json object:  {"uri":"\/api\/scrape\/XX","symbol":"","price":"","annual_dividend":""} <br>
+example: {"uri":"\/api\/scrape\/SPYI","symbol":"SPYI","price":"48.41","annual_dividend":"5.76"} <br>
+
