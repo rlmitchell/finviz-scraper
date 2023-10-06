1f00ea3529bb9047eeec24d9f51f86d87f392911
Rob Mitchell <rlmitchell@gmail.com>
2023-10-05 20:15:16 -0500

    api/index.php

--- api/index.php
+++ api/index.php
@@ -1,4 +1,5 @@
 <?php
+$GLOBALS['version'] = "2023.10.05.1909";
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER['REQUEST_URI'] == '/api/docs' || $_SERVER['REQUEST_URI'] == '/api/'){
@@ -10,6 +11,12 @@ if($_SERVER['REQUEST_URI'] == '/api/docs' || $_SERVER['REQUEST_URI'] == '/api/')
 if(str_starts_with($_SERVER['REQUEST_URI'], '/api/scrape') ){
   header('Content-Type: application/json');
   include_once('scrape.php');
+  exit();
+}
+
+if(str_starts_with($_SERVER['REQUEST_URI'], '/api/version') ){
+  header('Content-Type: application/json');
+  include_once('version.php');
   exit();
 }
 
